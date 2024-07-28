0e0bd626990726f4681068502515845608ecd12f
Samuu <samuelfilipevianadois@gmail.com>
2024-07-27 21:50:24 -0300

    feat: variaveis de cor

---
+++ app/src/main/java/br/com/samuel/gym_app/ui/theme/Colors/Color.kt
@@ -0,0 +1,14 @@
+package br.com.samuel.gym_app.ui.theme.Colors
+
+import androidx.compose.ui.graphics.Color
+
+val OrangeLight = Color(0xFFFFA726)
+val OrangePrimary = Color(0xFFFF9800)
+val Primary = Color(0xFFFF7C43) // Laranja
+val Secondary = Color(0xFF003F5C) // Azul escuro
+val Blue = Color(0xFF2F4B7C)
+val Purple = Color(0xFF665191)
+val Pink = Color(0xFFA05195)
+val White = Color(0xFFFFFFFF)
+val DarkGray = Color(0xFF424242) // Cinza escuro para texto
+val Black = Color(0xFF000000)
